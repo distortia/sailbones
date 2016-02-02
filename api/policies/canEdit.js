ba80c21cc97c78bdeb9cc76649093e8aa0b4546f
neilnagarsheth <neilnagarsheth@gmail.com>
2016-02-01 21:25:39 -0500

    modifying some files for the git forking test

--- api/policies/canEdit.js
+++ api/policies/canEdit.js
@@ -2,7 +2,7 @@
 module.exports = function(req, res, ok){
 	if (req.session.authenticated && req.session.user.canEdit) return ok();
 	else{
-		req.session.flash = {requireLoginError: 'You must be signed in to access this page.'};
+		req.session.flash = {UserLoginError: 'You must be signed in to access this page.'};
 		res.redirect('/sandcastle/user/login');
 		return;
 	}
